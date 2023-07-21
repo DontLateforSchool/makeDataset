8b8cb78a1c0ce067fe6f68161f0b1c27e8b38de7
cpprhtn <cpprhtn@naver.com>
2023-07-21 23:08:52 +0900

    Update model & api

--- ML/bus_pred.py
+++ ML/bus_pred.py
@@ -26,7 +26,7 @@ def create_lstm_input(data, time_steps, next_day_steps):
         y.append(list(data[i+time_steps:i+time_steps+next_day_steps]))
     return np.array(X), np.array(y)
 
-'''
+
 def set_data():
     path = "./../2023"
     flist = os.listdir(path)
@@ -44,14 +44,14 @@ def set_data():
         datas.append(flattens)
 
     data = {
-        'Date': pd.date_range(start='2023-03-17', periods=80*17, freq='H'),  # 날짜 정보 (시작 날짜와 주기, n은 주차 수)
-        'Hour': list(range(6, 23)) * 80,  # 시간 정보 (6시부터 22시까지)
-        'Passengers': flatten_2d_list(datas),  # 해당 시간의 이용객 수
+        'Date': pd.date_range(start='2023-03-17', periods=80*17, freq='H'),
+        'Hour': list(range(6, 23)) * 80, 
+        'Passengers': flatten_2d_list(datas), 
     }
 
     df = pd.DataFrame(data)
     return df
-'''
+
 def model_eval():
     df = pd.read_excel("./../output.xlsx")
 
@@ -78,5 +78,36 @@ def model_eval():
     
 def model_pred(data):
     loaded_model = load_model("lstm_model")
+    y_pred = loaded_model.predict(data)
+    return y_pred[0]
+
+
+def w_model_eval():
+    df = pd.read_excel("./../output.xlsx")
+
+    time_steps = 17*5  # 하루에 해당하는 시간대 수
+    next_day_steps = 17*5  # 다음날 하루에 해당하는 시간대 수
+
+    X, y = create_lstm_input(df['Passengers'], time_steps, next_day_steps)
+
+    split_idx = int(len(X) * 0.9)
+    X_train, X_test = X[:split_idx], X[split_idx:]
+    y_train, y_test = y[:split_idx], y[split_idx:]
+
+    model = Sequential()
+    model.add(LSTM(64, input_shape=(time_steps, 1), return_sequences=True))
+    model.add(LSTM(64))
+    model.add(Dense(128))
+    model.add(Dense(64))
+    model.add(Dense(32))
+    model.add(Dense(next_day_steps))
+    model.compile(optimizer='adam', loss='mse')
+    model.fit(X_train, y_train, epochs=100, batch_size=17)
+    model.save("w_lstm_model")
+
+    return X_test
+    
+def w_model_pred(data):
+    loaded_model = load_model("w_lstm_model")
     y_pred = loaded_model.predict(data)
     return y_pred[0]
--- ML/fastapi/main.py
+++ ML/fastapi/main.py
@@ -7,6 +7,12 @@ from pydantic import BaseModel
 app = FastAPI()
 
 X_test = model_eval()
+w_X_test = w_model_eval()
+
+class week_time_IN(BaseModel):
+    times: int
+class week_time_Out(BaseModel):
+    wtime: list
 
 @app.get("/")
 async def root():
@@ -16,3 +22,13 @@ async def root():
 def read_users():
     pred = np.round(model_pred(X_test)).astype(int).tolist()
     return {'pred' : pred}
+
+@app.post("/w_pred", response_model = week_time_Out)
+def read_users(times: week_time_IN):
+    print(times)
+    time_pred = []
+    pred = np.round(w_model_pred(w_X_test)).astype(int).tolist()
+    for i in range(times.times - 6, len(pred), 17):
+        time_pred.append(pred[i])
+    print(time_pred)
+    return {'wtime' : time_pred}
